7c4e010b37f9a8d3e4f7a8c72e2773d4e0730ad0
suhail0310000 <suhail_0310@hotmail.com>
2023-11-05 17:29:57 +0100

    Paginating the results done

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,9 +9,9 @@ namespace SearchService.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+        public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm, int pageNumber = 1, int pageSize = 4)
         {
-            var query = DB.Find<Item>();
+            var query = DB.PagedSearch<Item>();
 
             query.Sort(x => x.Ascending(a => a.Make));
 
@@ -19,9 +19,17 @@ namespace SearchService.Controllers
             {
                 query.Match(Search.Full, searchTerm).SortByTextScore();
             }
+
+            query.PageNumber(pageNumber);
+            query.PageSize(pageSize);
             var result = await query.ExecuteAsync();
 
-            return result;
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
             //var query = DB.PagedSearch<Item, Item>();
 
             //if (!string.IsNullOrEmpty(searchParams.SearchTerm))
