3d0ff7672909a8e1045b01569f3e505b3062b031
suhail0310000 <suhail_0310@hotmail.com>
2023-11-05 16:01:26 +0100

    adding the item model done

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,4 +16,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+await DB.InitAsync("SearchDb", MongoClientSettings
+           .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
 app.Run();
