78dc3e73ca5e7d9b7fd0b79d55772ee8cb193cf5
suhail0310000 <suhail_0310@hotmail.com>
2023-11-10 20:38:32 +0100

    adding a message outbox done

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using AuctionService.Entities;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Data
@@ -11,5 +12,15 @@ namespace AuctionService.Data
         }
 
         public DbSet<Auction> Auctions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Outbox functionality regarding if some the services are down. Needed for data consistency
+            modelBuilder.AddInboxStateEntity();
+            modelBuilder.AddOutboxMessageEntity();
+            modelBuilder.AddOutboxStateEntity();
+        }
     }
 }
