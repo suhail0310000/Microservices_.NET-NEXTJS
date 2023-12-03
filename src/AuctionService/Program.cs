c150b46f5291ad8386984884fbd259a8887254b8
suhail0310000 <suhail_0310@hotmail.com>
2023-12-03 15:48:54 +0100

    added auctionrepo and auction interface

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.ValidateAudience = false;
         options.TokenValidationParameters.NameClaimType = "username";
     });
-
+builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
