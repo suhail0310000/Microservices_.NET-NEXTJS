2e25ddf34c06ae77e5e523257e9e93d89f59dea6
suhail0310000 <suhail_0310@hotmail.com>
2024-01-17 13:26:43 +0100

    Integrations testing setup

---
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionService.IntegrationTests.Fixtures
+{
+    public class CustomWebAppFactory : WebApplicationFactory<Program>
+    {
+
+    }
+}
