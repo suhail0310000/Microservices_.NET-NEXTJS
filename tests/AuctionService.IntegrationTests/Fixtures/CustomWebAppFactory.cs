1fda1176d3e0d4f666955800ecf295bd6163bc73
suhail0310000 <suhail_0310@hotmail.com>
2024-01-18 08:48:34 +0100

    adding test data to the DB

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,14 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AuctionService.Data;
+using AuctionService.IntegrationTests.Util;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using WebMotions.Fake.Authentication.JwtBearer;
 
-namespace AuctionService.IntegrationTests.Fixtures
+namespace AuctionService.IntegrationTests;
+
+public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
-    public class CustomWebAppFactory : WebApplicationFactory<Program>
-    {
+    private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
 
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
     }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveDbContext<AuctionDbContext>();
+
+            services.AddDbContext<AuctionDbContext>(options =>
+            {
+                options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+            });
+
+            services.AddMassTransitTestHarness();
+
+            services.EnsureCreated<AuctionDbContext>();
+
+            services.AddAuthentication(FakeJwtBearerDefaults.AuthenticationScheme)
+                .AddFakeJwtBearer(opt =>
+                {
+                    opt.BearerValueType = FakeJwtBearerBearerValueType.Jwt;
+                });
+        });
+    }
+
+    Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
+}
+
+internal class PostgresSqlContainer
+{
 }
---
+++ tests/AuctionService.IntegrationTests/Util/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionService.IntegrationTests.Util
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RemoveDbContext<T>(this IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+
+            if (descriptor != null) services.Remove(descriptor);
+        }
+
+        public static void EnsureCreated<T>(this IServiceCollection services)
+        {
+            var sp = services.BuildServiceProvider();
+
+            using var scope = sp.CreateScope();
+
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<AuctionDbContext>();
+
+            db.Database.Migrate();
+            DbHelper.InitDbForTests(db);
+        }
+
+    }
+}
