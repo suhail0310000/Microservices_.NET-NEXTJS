75cc5082aaf825597ac7c35c405cca29ef3bfd45
suhail0310000 <suhail_0310@hotmail.com>
2024-01-29 13:50:56 +0100

    refactoring our code to use the zustand state

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'
+import { useParamsStore } from '../hooks/useParamsStore';
 
 type Props = {
     pageSize: number;
@@ -8,23 +9,25 @@ type Props = {
 
 const pageSizeButtons = [4, 8, 12];
 
-export default function Filters({pageSize, setPageSize}: Props) {
-  return (
-    <div className='flex justify-between items-center mb-4'>
-        <div>
-            <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
-                <ButtonGroup>
-                    {pageSizeButtons.map((value, i) => (
-                        <Button key={i}
-                            onClick={() => setPageSize(value)}
-                            color={`${pageSize === value ? 'red' : 'gray'}`}
-                            className='focus:ring-0'
-                        >
-                            {value}
-                        </Button>
-                    ))}
-                </ButtonGroup>
+export default function Filters() {
+    const pageSize = useParamsStore(state => state.pageSize);
+    const setParams = useParamsStore(state => state.setParams);
+    return (
+        <div className='flex justify-between items-center mb-4'>
+            <div>
+                <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
+                    <ButtonGroup>
+                        {pageSizeButtons.map((value, i) => (
+                            <Button key={i}
+                                onClick={() => setParams({pageSize: value})}
+                                color={`${pageSize === value ? 'red' : 'gray'}`}
+                                className='focus:ring-0'
+                            >
+                                {value}
+                            </Button>
+                        ))}
+                    </ButtonGroup>
+            </div>
         </div>
-    </div>
   )
 }
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -6,31 +6,42 @@ import AppPagination from '../components/AppPagination';
 import { useState } from 'react';
 import { getData } from '../actions/auctionActions';
 import Filters from './Filters';
+import { shallow } from 'zustand/shallow';
+import qs from 'query-string';
+import { useParamsStore } from '../hooks/useParamsStore';
 
 export default function Listings() {
-  const [auctions, setAuctions] = useState<Auction[]>([]);
-  const [pageCount, setPageCount] = useState(0);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [pageSize, setPageSize] = useState(4);
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(state => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+  }), shallow)
+
+  const setParams = useParamsStore(state => state.setParams);
+  const url = qs.stringifyUrl({ url: '', query: params })
+
+  function setPageNumber(pageNumber: number) {
+      setParams({ pageNumber })
+  }
 
   useEffect(() => {
-      getData(pageNumber, pageSize).then(data => {
-        setAuctions(data.results);
-        setPageCount(data.pageCount);
-      })
-  }, [pageNumber, pageSize])
+    getData(url).then(data => {
+        setData(data);
+    })
+  }, [url])
 
-  if(auctions.length === 0) return <h3>Loading....</h3>
+  if (!data) return <h3>Loading...</h3>
   return (
     <>
-    <Filters pageSize={pageSize} setPageSize={setPageSize} />
+    <Filters />
       <div className='grid grid-cols-4 gap-6'>
-        {auctions.map(auction => (
+        {data.results.map(auction => (
           <AuctionCard auction={auction} key={auction.id}/>
         ))}
       </div>
       <div className='flex justify-center mt-4'>
-        <AppPagination pageChanged={setPageNumber} currentPage={pageNumber} pageCount={pageCount}/>
+        <AppPagination pageChanged={setPageNumber} currentPage={params.pageNumber} pageCount={data.pageCount}/>
       </div>
     </>
   );
