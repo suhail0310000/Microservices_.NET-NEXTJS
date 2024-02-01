da41adafcc22a2e595e1cd03353d163986ad9fba
suhail0310000 <suhail_0310@hotmail.com>
2024-02-01 11:24:34 +0100

    end of section 8 ---- second commit

--- frontend/web-app/app/actions/auctionActions.ts
+++ frontend/web-app/app/actions/auctionActions.ts
@@ -2,7 +2,7 @@ import { PagedResult, Auction } from "../types";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
     try {
-        const res = await fetch(`http://localhost:7002/api/search?${query}`);
+        const res = await fetch(`http://localhost:6001/search${query}`);
         if (!res.ok) {
             throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
         }
