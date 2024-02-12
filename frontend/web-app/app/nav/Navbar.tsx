c3e95966fd046d934b3019b5be36868e38e4d655
suhail0310000 <suhail_0310@hotmail.com>
2024-02-12 12:37:53 +0100

    populating the User actions dropdown

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -7,12 +7,6 @@ import UserActions from './UserActions'
 export default async function Navbar() {
   const user = await getCurrentUser();
   return (
-    // <header className='
-    //   sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md bg-white p-5'>
-    //     <Logo/>
-    //     <Search/>
-    //     <LoginButton/>
-    // </header>
     <header className='
       sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md
     '>
