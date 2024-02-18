66aabea28fe2846957fd3230d1e4da2909c6e44f
suhail0310000 <suhail_0310@hotmail.com>
2024-02-19 00:33:50 +0100

    Adding the Create auction server Action

--- frontend/web-app/app/actions/authActions.ts
+++ frontend/web-app/app/actions/authActions.ts
@@ -1,21 +1,35 @@
 import { getServerSession } from "next-auth";
 import { options } from '@/app/options'
+import { getToken } from "next-auth/jwt";
+import {cookies, headers} from 'next/headers';
+import { NextApiRequest } from "next";
 
-export async function getSession(){
+export async function getSession() {
     return await getServerSession(options);
 }
 
-export async function getCurrentUser(){
-    try{
+export async function getCurrentUser() {
+    try {
         const session = await getSession();
 
-        console.log(session);
+        if (!session) return null;
 
-        if(!session) return null;
+        return session.user
 
-        return session.user;
-    }
-    catch(error){
+    } catch (error) {
         return null;
     }
+}
+
+export async function getTokenWorkaround() {
+    const req = {
+        headers: Object.fromEntries(headers() as Headers),
+        cookies: Object.fromEntries(
+            cookies()
+                .getAll()
+                .map(c => [c.name, c.value])
+        )
+    } as NextApiRequest;
+
+    return await getToken({req});
 }
