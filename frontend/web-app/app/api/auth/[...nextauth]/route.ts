4e5f829fcddea9d0190902ed106c1c8ebbc7e4b9
suhail0310000 <suhail_0310@hotmail.com>
2024-03-28 22:45:48 +0100

    Preparing the client app

--- frontend/web-app/app/api/auth/[...nextauth]/route.ts
+++ frontend/web-app/app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,3 @@
-// import NextAuth, { NextAuthOptions } from "next-auth"
-// import DuendeIdentityServer6 from 'next-auth/providers/duende-identity-server6';
-
-// export const authOptions: NextAuthOptions = {
-//     session: {
-//         strategy: 'jwt'
-//     },
-//     secret:'blabla',
-//     providers: [
-//         DuendeIdentityServer6({
-//             id: 'id-server',
-//             clientId: 'nextApp',
-//             clientSecret: 'secret',
-//             issuer: 'http://localhost:5000',
-//             authorization: {params: {scope: 'openid profile auctionApp'}},
-//             idToken: true
-//         })
-//     ]
-// }
-
-// const handler = NextAuth(authOptions)
-// export { handler as GET, handler as POST }
-
 import NextAuth, { NextAuthOptions } from "next-auth"
 import DuendeIdentityServer6 from 'next-auth/providers/duende-identity-server6';
 import { custom } from 'openid-client';
@@ -38,7 +15,7 @@ export const authOptions: NextAuthOptions = {
             id: 'id-server',
             clientId: 'nextApp',
             clientSecret: 'secret',
-            issuer: 'http://localhost:5000',
+            issuer: process.env.ID_URL,
             authorization: {params: {scope: 'openid profile auctionApp'}},
             idToken: true
         })
--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -20,10 +20,9 @@ export default function SignalRProvider({ children, user }: Props) {
     const [connection, setConnection] = useState<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
-    // const apiUrl = process.env.NODE_ENV === 'production' 
-    //     ? 'https://api.carsties.store/notifications'
-    //     : process.env.NEXT_PUBLIC_NOTIFY_URL
-    const apiUrl = 'http://localhost:6001/notifications'
+    const apiUrl = process.env.NODE_ENV === 'production' 
+        ? 'https://api.carsties.store/notifications'
+        : process.env.NEXT_PUBLIC_NOTIFY_URL
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
